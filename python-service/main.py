48d57e9a615123a9531ff3bb80c484bbd982c695
Bob Lin <smallgiant1010@gmail.com>
2024-12-24 22:54:22 -0500

    data extraction complete

--- python-service/main.py
+++ python-service/main.py
@@ -1,6 +1,12 @@
-from attr import dataclass
+from datetime import datetime
 import gspread
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from google.oauth2.service_account import Credentials
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service
+import time
 
 scopes = [
     "https://www.googleapis.com/auth/spreadsheets.readonly",
@@ -12,16 +18,96 @@ creds = Credentials.from_service_account_file("./secretKey.json", scopes=scopes)
 client = gspread.authorize(credentials=creds) # type: ignore
 sheet = client.open("Copy of Fall 2024 BTHS Discord Crowdsourced Course/Teacher Experience Survey (Responses)").sheet1
 
-@dataclass
-class commentObject:
-    Timestamp: str
-    CourseName: str
-    YearsTaken: str
-    Experience: str
-    Difficulty: int
-    WorkLoad: int
-    Advice: int
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["GET"],
+)
 
-data = sheet.get_all_records()
-[print(row) for row in data]
+
+def scrape_website(website):
+    edge_driver_path = "./msedgedriver.exe"
+    options = webdriver.EdgeOptions()
+    driver = webdriver.Edge(service=Service(edge_driver_path), options=options)
+    try:
+        driver.get(website)
+        print("Page Loaded...")
+        driver.implicitly_wait(5)
+        html = driver.page_source
+        time.sleep(5)
+        return html
+    finally:
+        driver.quit()
+
+def extract_staff_content(html_content, storage):
+    soup = BeautifulSoup(html_content, 'html5lib')
+    staff_div = soup.find("div", { "id" : "staff" })
+    if not staff_div: 
+        return storage
+    else:
+        for category_div in staff_div.find_all("div", { "class" : "staff-category"}): 
+            for staff_list in category_div.find_all("ul", { "class": "staff-categoryStaffMembers" }):
+                for staff_member in staff_list.find_all("li", { "class" : "staff-categoryStaffMember" }):
+                    temp = {}
+                    
+                    img_tag = staff_member.find("img", class_="staffPhoto lazy")
+                    if img_tag:
+                        src_img = img_tag.get('src')
+                        temp["img"] = f"https://www.bths.edu/{src_img}"
+                    else:
+                        temp["img"] = "https://www.bths.edu/apps/pics/blank_profile_public.png"
+                    
+                    name_tag = staff_member.find("dt")
+                    if name_tag:
+                        temp["name"] = name_tag.get_text(strip=True)
+                    
+                    occupation_tag = staff_member.find("dd")
+                    if occupation_tag:
+                        temp["occupation"] = occupation_tag.get_text(strip=True)
+                    
+                    if temp:
+                        storage.append(temp)
+
+    return storage 
+
+
+@app.get("/api/spreadsheet")
+def get_all_data():
+    data = sheet.get_all_records()
+    new_data = []
+    for obj in data:
+        _createdAt = datetime.strptime(str(obj["Timestamp"]), '%m/%d/%Y %H:%M:%S')
+        courseName = str(obj["Course Name"]).rstrip().upper()
+        years = obj["School year taken (i.e. 2023-24, 2022-23)"]
+        teacher_name_stringified = str(obj["Teacher name"])
+        teacherName = teacher_name_stringified[teacher_name_stringified.find(" ")+1:].upper() if teacher_name_stringified.find(" ") != -1 else teacher_name_stringified.upper()
+        experience = obj["How was your experience?"]
+        difficulty = obj["Difficulty rating (1-10)"]
+        workload = obj["Workload (1-10)"]
+        advice = obj["Any advice you would share?"]
+        resources = obj["Any resources you would recommend?"]
+        temp = {
+            "createdAt" : _createdAt,
+            "updatedAt" : _createdAt,
+            "courseName" : courseName,
+            "years": years,
+            "teacherName": teacherName,
+            "experience": experience,
+            "difficulty": difficulty,
+            "workload": workload,
+            "advice": advice,
+            "resources": resources,
+            "user_email": "anonymous",
+        }
+        new_data.append(temp)
+    return new_data
+
+@app.get("/api/full_faculty")
+def get_all_faculty():
+    html_content = scrape_website("https://www.bths.edu/apps/staff/")
+    return extract_staff_content(html_content, [])
+
+
 
