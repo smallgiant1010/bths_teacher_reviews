84cc8aa0553049a700ac726a4780a200cea3963f
Bob Lin <smallgiant1010@gmail.com>
2024-12-26 23:03:21 -0500

    initialized frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,17 @@
-
-
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <div className="pages">
+          <Routes>
+            <Route
+              path="/"
+              // element={}
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
