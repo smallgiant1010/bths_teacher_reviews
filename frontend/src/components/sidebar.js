063b19368a9f1a8e117e6117d913dc5291b5312e
Harry <weijial2@nycstudents.net>
2024-12-28 17:08:59 -0500

    mucho files

--- frontend/src/components/sidebar.js
+++ frontend/src/components/sidebar.js
@@ -1,16 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./sidebar.css"
-import { FaBars } from "react-icons/fa"
+import { CgMenu } from "react-icons/cg";
 import { Link } from "react-router-dom"
 
-function sidebar() {
-  return (
-    <div class="sidebar">
-      <Link to="#" class="menu-bars">
-        <FaBars/>
-      </Link>
-    </div>
-  )
+function Sidebar() {
+    const [collapsed, setCollapsed] = useState(false)
+    const toggleSidebar = () => { setCollapsed(!collapsed) }
+
+    const [teachers, get_all_teachers] = useState(null)
+        useEffect(() => {
+            const fetchTeachers = async () => {
+                try { 
+                    const response = await fetch("/api/teachers/") 
+                    const content = await response.json() 
+                    if (response.ok) {
+                        console.log("Fetched teachers: ", content)
+                        get_all_teachers(content)
+                    } else { 
+                        console.error("Failed to fetch teachers")
+                    } 
+                } catch (error) { 
+                    console.error("Error fetching teachers: ", error)
+                }
+            }
+           
+            fetchTeachers()
+        }, [])
+    
+        const set = new Set()
+        if (teachers) { 
+            teachers.forEach(teacher => { set.add(teacher.category) })
+        } 
+        const categories = Array.from(set)
+
+    return (
+        <div id={`sidebar${collapsed?'collapsed':''}`}>
+            <button type="button" onClick={toggleSidebar} id="menu-button">
+                <CgMenu/> 
+            </button>
+            {!collapsed && ( 
+                <div className="sidebar-shortcuts"> 
+                {categories.map(cat => (
+                    <div key={`${cat}-shortcut`}><Link to={cat}>{cat}</Link></div>
+                ))} 
+                </div> 
+            )}
+        </div>
+    )
 }
 
-export default sidebar
+export default Sidebar
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -5,10 +5,17 @@ const Home = () => {
     const [teachers, get_all_teachers] = useState(null)
     useEffect(() => {
         const fetchTeachers = async () => {
-            const response = await fetch("/api/teachers/")
-            const content = await response.json();
-            if (response.ok) {
-                get_all_teachers(content)
+            try { 
+                const response = await fetch("/api/teachers/") 
+                const content = await response.json() 
+                if (response.ok) {
+                    console.log("Fetched teachers: ", content)
+                    get_all_teachers(content)
+                } else { 
+                    console.error("Failed to fetch teachers")
+                } 
+            } catch (error) { 
+                console.error("Error fetching teachers: ", error)
             }
         }
        
@@ -16,23 +23,31 @@ const Home = () => {
     }, [])
 
     const set = new Set()
-    teachers.forEach(teacher => { set.add(teacher.category) })
+    if (teachers) { 
+        teachers.forEach(teacher => { set.add(teacher.category) })
+    } 
     const categories = Array.from(set)
 
     return (
         <div className="home">
-            <div className="searchbar">
-                <input type="text" placeholder="Search.."></input>
+            <div>
+                <input id="searchbar" type="text" placeholder="Search.."></input>
             </div>
 
             {categories.map(cat => (
                 <div key={cat}>
                     <h2 id={cat}>{cat}</h2>
+                    <div className="container">
                     {teachers && teachers
                     .filter(teacher => teacher.category == cat)
                     .map(teacher => (
-                        <div key={teacher.name} className="teacherProfile"><p className="teacherName">{teacher.name}</p><br/><p className="teacherPosition">{teacher.role}</p></div>
+                        <div key={teacher._id} className="teacherProfile">
+                            <img src={teacher.img_url} alt="image"/>
+                            <p className="teacherName">{teacher.name}</p>
+                            <p className="teacherPosition">{teacher.role}</p>
+                        </div>
                     ))}
+                    </div>
                 </div>
             ))}
 
