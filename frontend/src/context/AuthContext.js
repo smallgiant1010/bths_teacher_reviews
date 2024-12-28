547905724178c053b25833d34aaf942e0e8b7bef
Bob Lin <smallgiant1010@gmail.com>
2024-12-27 23:30:11 -0500

    Auth Context Setup

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,14 +1,30 @@
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useReducer } from "react";
+import { useCookies } from 'react-cookie';
 
 export const AuthContext = createContext();
 
 const AuthReducer = (state, action) => {
-
+    switch(action.type) {
+        case 'LOGIN':
+            return { user: action.payload };
+        case 'LOGOUT':
+            return { user: null };
+        default:
+            return state;
+    }
 };
 
 export const AuthContextProvider = ({ children }) => {
     const [state, dispatch] = useReducer(AuthReducer, {
         user: null,
+    });
+
+    useEffect(() => {
+        const [cookies, setCookie, removeCookie] = useCookies(['jwt']);
+        const token = cookies.jwt;
+        if(token) {
+            dispatch({type: 'LOGIN', payload: token});
+        }
     });
 
     return (
