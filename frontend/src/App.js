156942d4ce8ef81dbcfbe7aae8fb069fff303b77
Harry <weijial2@nycstudents.net>
2025-01-09 17:44:09 -0500

    minor change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import "./App.css"
 import Sidebar from "./components/sidebar"
 import Home from "./pages/Home"
 
 function App() {
+  const [collapsed, setCollapsed] = useState(false)
   return (
     <div className="App">
       <BrowserRouter>
-      <Sidebar />
+      <Sidebar collapsed={collapsed} setCollapsed={setCollapsed} />
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />}/>
