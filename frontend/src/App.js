2b8e9b1a359554304f21ec0222df93294002d778
Harry <weijial2@nycstudents.net>
2025-01-23 16:50:21 -0500

    restructure front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,51 @@
 import React, { useState } from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import "./App.css"
-import Sidebar from "./components/sidebar"
-import Home from "./pages/Home"
+import "./css/App.css"
+import Sidebar from "./components/sidebar.js"
+import Home from "./pages/Home.js"
+import Login from "./pages/Login.js"
+import Error404 from "./pages/PageNotFound.js"
+import TeacherDetails from "./pages/TeacherPages.js"
+import AdminDashboard from "./pages/AdminDashboard.js"
+import { AuthContextProvider, AuthContext } from "./context/AuthContext.js"
+
 
 function App() {
-  const [collapsed, setCollapsed] = useState(false)
-  return (
-    <div className="App">
-      <BrowserRouter>
-      <Sidebar collapsed={collapsed} setCollapsed={setCollapsed} />
-        <div className="pages">
-          <Routes>
-            <Route path="/" element={<Home />}/>
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </div>
-  );
+ const [collapsed, setCollapsed] = useState(false)
+ return (
+   <div className="App">
+     <BrowserRouter>
+     <Sidebar collapsed={collapsed} setCollapsed={setCollapsed} />
+       <div className="pages">
+         <Routes>
+           <Route path="/" element={<Home />} />
+           <Route path="/login" element={<Login />} />
+           <Route
+             path="/teacher/:id"
+             element={
+               role === 'LOGIN' ? <TeacherDetails /> : <Navigate to="/login" />
+             }
+           />
+           <Route
+             path="/admin"
+             element={
+               role === 'ADMIN' ? <AdminDashboard /> : <Navigate to="/login" />
+             }
+           />
+           <Route path="*" element={<Error404 />} />
+         </Routes>
+       </div>
+     </BrowserRouter>
+   </div>
+ )
+}
+function MainApp() {
+ return (
+     <AuthContextProvider>
+         <App />
+     </AuthContextProvider>
+ );
 }
 
-export default App
+
+export default MainApp;
--- frontend/src/components/sidebar.js
+++ frontend/src/components/sidebar.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import "./sidebar.css"
+import "../css/sidebar.css"
 import { CgMenu } from "react-icons/cg"
 
 function Sidebar() {
